8b846119aea8d2e8faa84c226a455c4f09d4c60b
“Omar <omarelkashef01@gmail.com>
2023-07-10 19:33:06 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "./Header"
+
+function App(){
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>
+}
+
+export default App;
