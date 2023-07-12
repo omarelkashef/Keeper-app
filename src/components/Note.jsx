09c23e439adba1932408367493b039bb6291b6cc
“Omar <omarelkashef01@gmail.com>
2023-07-12 16:03:01 -0500

    using hooks

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 function Note(props){
-    return <div className="note">
+    const [mouse , setMouse] = useState(false);
+    
+    function handleMouseOver(){
+        setMouse(true);
+    }
+    
+    function handleMouseOut(){
+        setMouse(false);
+    }
+    return <div onMouseOver={handleMouseOver} onMouseOut= {handleMouseOut}
+                className="note"
+                style={{backgroundColor: mouse ? "#f5ba13" : null}}
+            >
         <h1>{props.title}</h1>
         <p>{props.content}</p>
     </div>;
